bf328f5b3153201f1a30f8dd96feb4de5fb51d2d
Yuto.K <kairi3108@i.softbank.jp>
2016-10-20 00:14:52 +0900

    Initial Commit

---
+++ YDAStringGrepper/YDAStringGrepper.h
@@ -0,0 +1,19 @@
+//
+//  YDAStringGrepper.h
+//  YDAStringGrepper
+//
+//  Created by Yuto on 2016/10/20.
+//  Copyright © 2016年 Yuto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YDAStringGrepper.
+FOUNDATION_EXPORT double YDAStringGrepperVersionNumber;
+
+//! Project version string for YDAStringGrepper.
+FOUNDATION_EXPORT const unsigned char YDAStringGrepperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YDAStringGrepper/PublicHeader.h>
+
+
